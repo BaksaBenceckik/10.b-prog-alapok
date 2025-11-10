17ccb7440895a681fe637de8f3ad7a7029d0c7f5
Baksa Bence <baksa.bence@ckik.loc>
2025-11-10 11:31:38 +0100

    ciklus gyak

--- ciklusok gyak.py
+++ ciklusok gyak.py
@@ -1,2 +1,15 @@
-# Generáljon ki 10db véletlen számot!
+import random
+
+# Generáljon ki 10db egy jegyű véletlen számot!
 # Írassa ki a számok összegét!
+
+osszeg = 0
+for a in range(0, 11, 1):
+    velSzam = random.randint(1,9)
+    osszeg += velSzam
+    print(velSzam, end=" ")
+print( )
+print("Összeg: ",osszeg)
+
+# Hány db páros véletlen számot generált ki?
+# Melyik a legnagyobb véletlen szám?
