f4bfa0d74b274e241e33b56a2a5f536647b4b4e5
Baksa Bence <baksa.bence@ckik.loc>
2025-11-21 11:11:27 +0100

    gyak

--- gyakorlsá.py
+++ gyakorlsá.py
@@ -1,11 +1,26 @@
 # 1. Kérjen be egy szöveget és egy betűt! Adja meg hány db betű van a szövegben!
 szoveg = input("szoveg: ")
 betu = input("betu: ")
-for i in range(0, len(szoveg), 1):
-    print(i, end=" ")
-print()
-for index in range(0, len(betu), 1):
-    print(index, end=" ")
-print()
+mennyiseg = 0
+for i in range(len(szoveg)):
+    mennyiseg += 1
+print(mennyiseg)
+mennyiseg2 = 0
+for i in range(len(betu)):
+    mennyiseg2 += 1
+print(mennyiseg2)
+#for i in range(0, len(szoveg), 1):
+#    print(i, end=" ")
+#print()
+#for index in range(0, len(betu), 1):
+#    print(index, end=" ")
+#print()
 
+"""
+db = 0
+for karakter in szoveg:
+    if(karakter == betu):
+        db += 0
+        print(db)
+"""
 # Van e benne betű és ha van, hány db? 
