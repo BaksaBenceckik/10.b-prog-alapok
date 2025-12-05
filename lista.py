abc620b20bafad170d9066905d26978d54c0a3bf
Baksa Bence <baksa.bence@ckik.loc>
2025-12-05 11:30:53 +0100

    lista

--- lista.py
+++ lista.py
@@ -1,3 +1,5 @@
+import random as r
+
 # a szövegben van-e "sz" betű?
 
 szoveg = "aszály"
@@ -16,3 +18,35 @@ if index < len(szoveg)-1:
     print("Van benne", dube, "betű!")
 else:
     print("Nincs benne", dube, "betű!")
+
+"""
+lista -dinamikus
+- tudunk bele új elemet ralkni, ezzel nő az elem száma
+- tudunk belőle törölni, ezzel csökken az elemszáma
+- lekérhető bármelyik eleme
+- módosítható bármelyik eleme
+deklarálás + inicializálás:
+létrehozás + kezdőérték adás:
+lista_neve = []
+új elem hozzáadása:
+lista_neve.append(ujelem)
+elem törlése:
+lista_neve.remove(elem)
+beégetett lista:
+lista_neve = [3,2,5,7,1]
+lista hossza:
+len(lista_neve)
+"""
+számok = [3,2,5,7,1]
+számok.append(12)
+print("Első elem: ", számok[0])
+számok.remove(3)
+print("Utlsó elem: ", számok[len(számok)-1]) # könnyebb: számok[-1]
+print("Hossza: ", len(számok))
+
+# HF.: Tölts fel egy 13 elemű listát [0, 20]
+# Számok világa
+# Hány db páros szám van a listában
+# van-e benne nulla?
+
+házi = [r.randint(0,20)]
